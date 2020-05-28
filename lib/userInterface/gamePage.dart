f2be54faa6bd79133dd20bf91bc30e0d09dbb0c7
Marcelxy <marcel.geirhos@gmail.com>
2020-05-28 20:23:09 +0200

    Spielergebnisse anzeigen
    - Erste Spielergebnisse nach einer Runde werden angezeigt und es wird angezeigt welche Antworten der Benutzer richtig und falsch beantwortet hat => Spielablauf muss noch optimiert werden, dies ist die erste Version um eine komplette Spielrunde spielen zu können.

--- lib/userInterface/gamePage.dart
+++ lib/userInterface/gamePage.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'dart:async';
+import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
@@ -13,9 +14,11 @@ class GamePage extends StatefulWidget {
 
 class _GamePageState extends State<GamePage> {
   List<String> _answerKeyList;
+  List<dynamic> _answerValueList;
   List<String> _resultList;
   List<bool> _answerVisibility;
-  List<dynamic> _answerValueList;
+  List<Icon> _icon;
+  LinkedHashMap _solutionMap;
   int _selectedAnswerIndex;
   int _selectedResultIndex;
   int _answerCounter;
@@ -23,7 +26,10 @@ class _GamePageState extends State<GamePage> {
   Timer _timer;
   Future _loadQuestion;
   var _question;
+  Map _solution;
   Color _countdownColor;
+  bool _showResults;
+  String _nextStepButtonText;
 
   @override
   void initState() {
@@ -49,14 +55,14 @@ class _GamePageState extends State<GamePage> {
             return Column(
               children: <Widget>[
                 Padding(
-                  padding: const EdgeInsets.only(top: 44.0),
+                  padding: const EdgeInsets.only(top: 36.0),
                   child: Text(
                     '$_countdown',
                     style: TextStyle(fontSize: 24.0, letterSpacing: 1.8, color: _countdownColor),
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+                  padding: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 12.0),
                   child: Text(
                     _question['questionText'],
                     textAlign: TextAlign.center,
@@ -81,11 +87,10 @@ class _GamePageState extends State<GamePage> {
   Widget answerArea() {
     return Column(
       children: List.generate(
-        _answerKeyList.length.round(),
+        _answerKeyList.length.round() - 3,    // TODO warum so? Warum - 3 ud round()?
         (index) {
           return Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: List.generate(
               2,
               (index2) {
@@ -145,25 +150,51 @@ class _GamePageState extends State<GamePage> {
           children: List.generate(
             _question['answers'].length,
             (index) {
-              return GestureDetector(
-                onTap: () => setSelectedText(index),
-                child: ChoiceChip(
-                  label: Container(
-                    width: 150,
-                    child: AutoSizeText(
-                      _resultList[index]?.toString() ?? '',
-                      textAlign: TextAlign.center,
-                      maxLines: 2,
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Container(
+                    child: Visibility(
+                      visible: _showResults,
+                      maintainSize: true,
+                      maintainAnimation: true,
+                      maintainState: true,
+                      child: _showResults ? _icon[index] : Icon(Icons.close),
+                    ),
+                    padding: EdgeInsets.only(right: 70.0),
+                  ),
+                  GestureDetector(
+                    onTap: () => setSelectedText(index),
+                    child: ChoiceChip(
+                      label: Container(
+                        width: 150,
+                        child: AutoSizeText(
+                          _resultList[index]?.toString() ?? '',
+                          textAlign: TextAlign.center,
+                          maxLines: 2,
+                        ),
+                      ),
+                      selected: _selectedResultIndex == index,
+                      selectedColor: Color(0xffffc107),
                     ),
                   ),
-                  selected: _selectedResultIndex == index,
-                  selectedColor: Colors.green,
-                ),
+                ],
               );
             },
           ),
         ),
         Text(_question['bottomText']),
+        Visibility(
+          visible: _showResults,
+          maintainSize: true,
+          maintainAnimation: true,
+          maintainState: true,
+          child: RaisedButton(
+            onPressed: () => nextStep(),
+            child: Text(_nextStepButtonText),
+          ),
+        )
       ],
     );
   }
@@ -217,7 +248,7 @@ class _GamePageState extends State<GamePage> {
         () {
           if (_countdown < 1) {
             timer.cancel();
-            startNewRound();
+            showRightAndWrongAnswers();
           } else {
             if (_countdown <= 6) {
               _countdownColor = Colors.red;
@@ -232,7 +263,7 @@ class _GamePageState extends State<GamePage> {
   }
 
   /*
-
+   TODO Funktion kommentieren
    */
   Future<void> loadQuestionData() async {
     QuerySnapshot questionList = await Firestore.instance.collection('questions').getDocuments();
@@ -241,11 +272,10 @@ class _GamePageState extends State<GamePage> {
     _question = questionList.documents[randomNumber];
     _answerKeyList = _question.data['answers'].keys.toList();
     _answerValueList = _question.data['answers'].values.toList();
-    var answers = new Map();
+    _solution = new Map();
     for (int i = 0; i < _answerKeyList.length; i++) {
-      answers[_answerKeyList[i]] = _answerValueList[i];
+      _solution[_answerKeyList[i]] = _answerValueList[i];
     }
-    answers.entries.toList();
     _answerKeyList.shuffle();
     _countdown = 5 * _answerKeyList.length;
     _resultList = new List(_answerKeyList.length);
@@ -259,7 +289,41 @@ class _GamePageState extends State<GamePage> {
     startTimer();
   }
 
+  void showRightAndWrongAnswers() {
+    _showResults = true;
+    // Sortiert die Antwortenliste absteigend vom höchsten zum niedrigsten.
+    var sortedKeys = _solution.keys.toList(growable: false)..sort((k1, k2) => _solution[k2].compareTo(_solution[k1]));
+    _solutionMap = new LinkedHashMap.fromIterable(sortedKeys, key: (k) => k, value: (k) => _solution[k]);
+    _icon = new List(_resultList.length);
+    for (int i = 0; i < _resultList.length; i++) {
+      if (_resultList[i] == _solutionMap.keys.elementAt(i)) {
+        _icon[i] = Icon(Icons.done, color: Colors.green);
+      } else {
+        _icon[i] = Icon(Icons.close, color: Colors.red);
+      }
+    }
+  }
+
+  void nextStep() {
+    if (_nextStepButtonText == 'Ergebnisse anzeigen') {
+      showResults();
+    } else {
+      startNewRound();
+    }
+  }
+
+  void showResults() {
+    for (int i = 0; i < _resultList.length; i++) {
+      _resultList[i] = _solutionMap.keys.elementAt(i);
+    }
+    setState(() {
+      _nextStepButtonText = 'Nächste Frage';
+    });
+  }
+
   void startNewRound() {
+    _nextStepButtonText = 'Ergebnisse anzeigen';
+    _showResults = false;
     _answerCounter = 0;
     _selectedAnswerIndex = -1;
     _selectedResultIndex = -1;
